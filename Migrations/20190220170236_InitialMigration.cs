ea2c3b02ef4dfdea0cb2e2fe650d54ca50a52d81
Nathaniel Nunes <manutdkid77@gmail.com>
2019-02-21 01:01:54 +0530

    Create InitialMigration to set up database schema

---
+++ Migrations/20190220170236_InitialMigration.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TheMoviePlace.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GenderReferences",
+                columns: table => new
+                {
+                    GenderReferenceID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Type = table.Column<string>(maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GenderReferences", x => x.GenderReferenceID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    Plot = table.Column<string>(maxLength: 255, nullable: true),
+                    Poster = table.Column<string>(maxLength: 50, nullable: true),
+                    YearOfRelease = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoleReferences",
+                columns: table => new
+                {
+                    RoleReferenceID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Type = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleReferences", x => x.RoleReferenceID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    PersonID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Bio = table.Column<string>(maxLength: 2000, nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    GenderReferenceID = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.PersonID);
+                    table.ForeignKey(
+                        name: "FK_Persons_GenderReferences_GenderReferenceID",
+                        column: x => x.GenderReferenceID,
+                        principalTable: "GenderReferences",
+                        principalColumn: "GenderReferenceID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    RoleID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MovieID = table.Column<int>(nullable: false),
+                    PersonID = table.Column<int>(nullable: false),
+                    RoleReferenceID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.RoleID);
+                    table.ForeignKey(
+                        name: "FK_Roles_Movies_MovieID",
+                        column: x => x.MovieID,
+                        principalTable: "Movies",
+                        principalColumn: "MovieID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Roles_Persons_PersonID",
+                        column: x => x.PersonID,
+                        principalTable: "Persons",
+                        principalColumn: "PersonID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Roles_RoleReferences_RoleReferenceID",
+                        column: x => x.RoleReferenceID,
+                        principalTable: "RoleReferences",
+                        principalColumn: "RoleReferenceID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_GenderReferenceID",
+                table: "Persons",
+                column: "GenderReferenceID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_MovieID",
+                table: "Roles",
+                column: "MovieID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_PersonID",
+                table: "Roles",
+                column: "PersonID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_RoleReferenceID",
+                table: "Roles",
+                column: "RoleReferenceID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "RoleReferences");
+
+            migrationBuilder.DropTable(
+                name: "GenderReferences");
+        }
+    }
+}
