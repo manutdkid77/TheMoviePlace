a5865294233259fb4c3ba5bbb0298aed5ae1d145
Nathaniel Nunes <manutdkid77@gmail.com>
2019-02-21 01:00:58 +0530

    Set up sqlserver provider with local dev connection string

--- Startup.cs
+++ Startup.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TheMoviePlace.Entities;
 
 namespace TheMoviePlace
 {
     public class Startup
-    {
-        public Startup(IConfiguration configuration)
+    {   
+        public static IConfiguration ConfigurationSettings {get;set;}
+        public Startup(IConfiguration _configuration)
         {
-            Configuration = configuration;
+            ConfigurationSettings = _configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -22,6 +25,9 @@ namespace TheMoviePlace
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            var strDBConnection = ConfigurationSettings["connectionStrings:TheMoviePlaceDBConnectionString"];
+            services.AddDbContext<TheMoviePlaceDBContext>(o=>o.UseSqlServer(strDBConnection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
